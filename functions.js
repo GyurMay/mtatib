0db2e0af206b7fe8e1ef1c1abb97e745abd89820
GyurMay <creepatty91@gmail.com>
2021-08-23 12:21:06 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,64 @@
+/****
+ * 
+ *              FUNCTIONS BELOW - convert to a different js file later and import
+ */
+
+function tibetanName(origName)
+{
+    if(has(lang, '?lang=eng')) return origName;
+    if(engToTib[origName] == null || engToTib[origName] == '')
+        return origName;
+    return engToTib[origName];
+}
+
+function has(str, frac)
+{
+    if(str.split(frac).length > 1)
+        return true;
+    return false;
+}
+// loadSchedules(5); //rm
+
+function filter(){
+    console.log('filter clicked');
+    let visibleStation = document.getElementsByClassName('options')[0].childElementCount;
+    let search = searchField.value;
+    for(i=0;i<visibleStation;i++){ //goes through options class to hide all the options
+    if(document.getElementsByClassName('options')[0].children[i].id.toLowerCase().split(search.toLowerCase()).length >= 2){
+        match = document.getElementsByClassName('options')[0].children[i].id;
+        console.log('Results found: ' + match);
+        document.getElementById(match).style.display = 'block';
+    }else{
+        document.getElementsByClassName('options')[0].children[i].style.display = 'none';
+    }
+    }
+    if(match == ''){
+        console.log('Nothing found bih');
+        document.getElementById('noresultsfound').style.display = 'block';
+    }
+}
+
+function getClosestStation(lat, lon){
+        myloc = [];
+        myloc[0] = lat; console.log(lat);
+        myloc[1] = lon;
+        num = [];
+        for(i=0;i<document.getElementsByClassName("options")[0].childElementCount;i++){
+        num[i] = [parseFloat(document.getElementsByClassName("options")[0].children[i].getAttribute('lat')), parseFloat(document.getElementsByClassName("options")[0].children[i].getAttribute('lon'))];
+        }
+        d_arr = [];
+        d_arr_cop = [];
+        for(j=0;j<i;j++){ //logging the distances of the stations with increment j for objects
+        distance = Math.sqrt((((num[j][0] - myloc[0])*(num[j][0] - myloc[0])) + (num[j][1] - myloc[1])*(num[j][1] - myloc[1])));
+        console.log("from home to "+document.getElementsByClassName("options")[0].children[j].textContent + ". distance: "+distance);
+        d_arr[distance] = document.getElementsByClassName("options")[0].children[j].id; //input distance get the station Id
+        d_arr_cop[j] = distance;
+        }
+        d_arr_cop.sort();
+        minAddresses = []; //store the id of minimum distanced Stations
+        for(i=0, k=0; i<j; i++){
+        if(d_arr[0] == d_arr_cop[i]){
+        	minAddresses[k++] = (document.getElementsByClassName("options")[0].children[i].id);
+        }
+        }
+}
---
+++ schedules.js
@@ -0,0 +1,169 @@
+/***
+  FEATURES TO MAKE
+  1. Remove duplicates after loading twice or, restrict loading twice
+  2. Train load to Select button with options inside
+  3. Path feature (loadImmediatePath)
+  4. Design upgrade
+  5. gmaps integration
+  **/
+lang = document.location.search;
+locationPermission = false;
+function loadSchedules(line, k){
+
+//getting and parsing subway train times
+let url = `https://otp-mta-prod.camsys-apps.com/otp/routers/default/nearby?stops=MTASBWY:${line + (k.toString().padStart(2,'0')) }&apikey=Z276E3rCeTzOQEoBPPN4JCEc6GfvdnYE`;
+console.log(url);
+console.log(i);
+  
+if(k > 40){
+  
+  //set global const lat and lon
+  if(navigator.geolocation){
+  
+    if(locationPermission == false){
+    navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    globalLat = position.coords.latitude;
+    globalLon = position.coords.longitude;
+    locationPermission = true;
+    getClosestStation(globalLat, globalLon);
+
+    lenny = options[0].childElementCount;
+    for(incre=0; incre<lenny; incre++){
+    options[0].appendChild(document.getElementById(d_arr[d_arr_cop[incre]]));
+    }
+    });
+    }else{
+    getClosestStation(globalLat, globalLon);
+
+    lenny = options[0].childElementCount;
+    for(incre=0; incre<lenny; incre++){
+    options[0].appendChild(document.getElementById(d_arr[d_arr_cop[incre]]));
+    }
+
+  }
+  }else{
+  alert("Permission denied - location, can't get nearby stations");
+  //getClosestStation(40.73659234516563, -73.87575414076787);
+  
+  lenny = options[0].childElementCount;
+  for(incre=0; incre<lenny; incre++){
+  options[0].appendChild(document.getElementById(d_arr[d_arr_cop[incre]]));
+  }
+  }
+  //end of global
+  
+  return;
+}
+k++;
+//12 -> Grand Ave, 05 -> Jamaica Center - Parsons/Archer
+// if(i > 37) return; //rm
+fetch(url).then((response) => {
+return response.json();
+})
+.then((jsonResponse) => {
+console.log(jsonResponse);
+a = jsonResponse;
+let jr = a[0];
+if(jsonResponse.length == 0)
+    loadSchedules(line, k);
+else{
+var name = jr['stop']['name'];
+var lat = jr['stop']['lat'];
+var lon = jr['stop']['lon'];
+stations = document.createElement('p'); //select divId(<p></p>) box
+stations.style.display = 'none';
+stationName = document.createElement('div');
+stationName.innerHTML += '<b>'+name+'</b><br/><br/>';
+stations.append(stationName);
+stations.id = name+'-time';
+
+// homeexception = false;
+// homeexception = (jr['groups'][i]['headsign"] == 'Grand Av-Newtown') ? true : false;
+
+var options = document.querySelector('.options');
+optionsP = document.createElement('p');
+optionsP.setAttribute('lat', lat);
+optionsP.setAttribute('lon', lon);
+optionsP.id = name;
+optionsP.onclick = () => { 
+    schedulevisible(name+'-time');
+ };
+let tName = tibetanName(name);
+optionsP.innerHTML = tName;
+// if(name == 'Grand Av-Newtown'){
+//     optionsP.style.backgroundColor = '#564178';
+//     optionsP.style.color = '#fff';
+// }
+options.appendChild(optionsP);
+table = document.createElement('table');
+var shortNames = [], headsigns = [], arrivalTimes = [];
+for(i=0; i<jr['groups'].length; i++){ //loop over each route (train Lines)
+    shortNames[i] = jr['groups'][i]['route']['shortName'];
+    headsigns[i] = jr['groups'][i]['headsign'];
+    numberOfTrains = jr['groups'][i]['times'].length;
+  
+    for(j=0; j<numberOfTrains; j++){ //get arrival times rows
+  	  tr = document.createElement('tr');
+  	  table.appendChild(tr);
+  	  tr.innerHTML += (`<td><img src='https://new.mta.info/themes/custom/bootstrap_mta/images/icons/${shortNames[i]}.svg'></td>`);
+      tr.innerHTML += '<td>'+tibetanName(headsigns[i])+'</td>'; //Print HeadSigns (which side is it going)
+      d = new Date(jr['groups'][i]['times'][j]['arrivalFmt']);
+      h = (d.getHours() > 12) ?  d.getHours() - 12 : d.getHours();
+      if(h == 0) h = 12;
+      amPm = (d.getHours() > 12) ? 'PM' : 'AM';
+      arrivalTimes[j] =  h+':'+d.getMinutes()+amPm;
+      tr.innerHTML += (`<td style='font-size:60%;'>${arrivalTimes[j]} </td>`);
+    }
+    document.body.appendChild(stations);
+}
+  stations.appendChild(table);
+loadSchedules(line, k);
+}
+});
+}
+
+//load Using your geolocation
+function loadImmediatePath(){ 
+//for(i=0;i<options[0].childElementCount;i++){
+//  options[0].children[i].onclick = () => { loadImmediatePath(latLon[0], latLon[1], i); };
+//}
+immediatePath(globalLat, globalLon, 0);
+function immediatePath(lati, long, k){
+var totalString = '<hr/><br/>paths<br/>';
+if(k >= options[0].childElementCount) return;
+var fromLat = lati;
+var fromLon = long;
+let lat = options[0].children[k].getAttribute('lat');
+let lon = options[0].children[k].getAttribute('lon');
+
+fetch(`https://otp-mta-prod.camsys-apps.com/otp/routers/default/plan?apikey=Z276E3rCeTzOQEoBPPN4JCEc6GfvdnYE&toPlace=${lat},${lon}&fromPlace=${fromLat},${fromLon}`).
+then((resp) => { return (resp.json()) }).then((jResp) => {
+a = jResp;
+console.log(a);
+itineraries = a['plan']['itineraries'];
+for(i=0;i < itineraries.length;i++){
+    for(j=0;j < itineraries[i]['legs'].length;j++){
+        if(itineraries[i]['legs'][j]['mode'] == 'WALK') continue;
+        if(itineraries[i]['legs'][j]['mode'] == 'BUS'){
+          logo = itineraries[i]['legs'][j]['route'];
+        }else{
+          logo = `<img width=100 height=100 src='https://new.mta.info/themes/custom/bootstrap_mta/images/icons/${itineraries[i]['legs'][j]['route']}.svg'></img>`;
+        }
+        totalString += logo;
+        arrivalTime = new Date(itineraries[i]['legs'][j]['startTimeFmt']);
+        amPm = (arrivalTime.getHours() > 12) ? 'PM' : 'AM';
+        hours = (amPm == 'PM') ? arrivalTime.getHours() - 12 : arrivalTime.getHours();
+        timeFormatted = (hours +':'+ arrivalTime.getMinutes() + amPm );
+        innerData = timeFormatted + ' (' + itineraries[i]['legs'][j]['headsign'] + ') ' + itineraries[i]['legs'][j]['from']['name'] + '<br>' ;
+        totalString += innerData;
+    }
+    totalString += '<br/><br/><hr/>';
+}
+console.log(document.getElementById(options[0].children[k].id+"-time"));
+console.log("--"+totalString);
+document.getElementById(options[0].children[k].id+"-time").innerHTML += totalString;
+immediatePath(lati, long, ++k);
+});
+}
+} //end of loadImmediatePath()
