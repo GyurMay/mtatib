22c0cfad1b0f2862b304351979caacba9807c129
GyurMay <creepatty91@gmail.com>
2021-09-24 00:40:42 -0400

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -103,7 +103,7 @@ for(i=0; i<jr['groups'].length; i++){ //loop over each route (train Lines)
       if(h == 0) h = 12;
       amPm = (d.getHours() > 12) ? 'PM' : 'AM';
       arrivalTimes[j] =  h+':'+d.getMinutes()+amPm;
-      tr.innerHTML += (`<td style='font-size:60%;'>${((new Date()).getMinutes() > h) ? d.getMinutes() - (new Date()).getMinutes() : d.getMinutes() - (new Date()).getMinutes() + 60} min</td>`);
+      tr.innerHTML += (`<td style='font-size:60%;'>${((new Date()).getHours() > d.getHours()) ? d.getMinutes() - (new Date()).getMinutes() : d.getMinutes() - (new Date()).getMinutes() + 60} min</td>`);
     }
     document.body.appendChild(stations);
 }
