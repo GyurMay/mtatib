ef30459d13ca1cdc00166196a9a30073eeeeee82
GyurMay <creepatty91@gmail.com>
2022-03-15 19:34:52 -0400

    Update schedules.js

--- schedules.js
+++ schedules.js
@@ -13,7 +13,7 @@ function loadMainPage(line, k1){
   if(k1 >= 1 && k1>=lineJson.length) return;
  let url = `https://collector-otp-prod.camsys-apps.com/schedule/MTASBWY/stopsForRoute?apikey=qeqy84JE7hUKfaI0Lxm2Ttcm6ZA0bYrP&&routeId=MTASBWY:${line}`;
  fetch(url).then((a) => { return a.json(); }).then((l) => { lineJson = l; }).then((e) => {
-   console.log("k is: "+k);
+   console.log("k is: "+k1);
    for(k1;k1<=lineJson.length;k1++){ //< -> <=
     name = lineJson[k1].stopName;
   if(locDb[lineJson[k1]["stopName"]] == undefined) continue;
